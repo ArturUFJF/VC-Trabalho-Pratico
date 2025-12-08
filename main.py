ecd9e810472fb0a33c316443a0f0f6cf27124000
Vinícius Macuco <macuco.vinicius@estudante.ufjf.br>
2025-12-08 19:29:59 -0300

    main corrigido para melhor uso do wandB

--- main.py
+++ main.py
@@ -1,78 +1,78 @@
 import wandb
 import os
 from dotenv import load_dotenv
+from datetime import datetime
+
 
 load_dotenv()
-wand_key = os.getenv("WANDB_KEY")
-if not wand_key:
-    raise ValueError("Nenhuma API_KEY encontrada. Verifique o arquivo .env.")
-print(f"A chave do wandB foi carregada com sucesso!")
+wandb.login(key=os.getenv("WANDB_KEY"))
 
-wandb.login(wand_key)
 
-#coloque aqui os valores dos seu hiper parametros
-cnn_type = "" #coloque o nome da cnn que for fazer (vgg16 para a vgg16 pura,vgg16-tl para vgg16 com transfer learning, resnet50 para resnet50 densenet121 para a DenseNet121)
-batch = 0
-epochs = 0
-learning_rate = 0
-frozen_layers = 0
+def main():
+    #Define qual a rede a ser treinada ("vgg16" para a vgg16 da Lívia, "vgg16-tl" para a "vgg16" do Artur, "resnet50-tl" ou "densenet121-tl")
+    #Qualquer outro valor aqui treinara apenas a ImageNet.
+    cnn_type = "vgg16"
 
-if cnn_type == "vgg16":
+    # Hiperparâmetros
+    config = {
+        "architecture": cnn_type,
+        "dataset": "CIFAR-100",
+        "epochs": 5,
+        "batch_size": 32,
+        "learning_rate": 0.001,
+        "frozen_layers": 5 if "tl" in cnn_type else 0
+    }
+
+    # 1. INICIA A RUN, preparando para armazenar dados na wandB
     run = wandb.init(
-        entity="macuco-vinicius-ufjf",
         project="VC-Trabalho-Pratico",
-        config={
-            "architecture": "VGG16",
-            "dataset": "CIFAR-100",
-            "learning_rate": learning_rate,
-            "epochs": epochs,
-            "batch": batch
-        }
-    )
-    # coloque aqui a sua lógica para treinar a rede
-
-    run.log({"accuracy": 0, "loss": 0}) #torca aqui pelo sua mAP e loss
-    run.finish()
-else:
-    run = wandb.init(
         entity="macuco-vinicius-ufjf",
-        project="VC-Trabalho-Pratico",
-        config={
-            "architecture": cnn_type,
-            "dataset": "CIFAR-100",
-            "learning_rate": learning_rate,
-            "epochs": epochs,
-            "batch": batch,
-            "frozen_layers": frozen_layers
-        }
+        name=f"{cnn_type}-run-{datetime.now().strftime('%H%M')}",
+        config=config,
+        reinit=True  # Importante se for rodar várias seguidas no mesmo script
     )
-    # aqui vem a lógica do treinamento da imageNet
 
-    if(cnn_type == "vgg16-tl"):
-        #lógica da vgg16 com o transfer learning
+    try:
+        metrics = {}
 
-        #exportando os dados para o wandb
-        run.log({"accuracy": 0, "loss": 0})
+        # 2. CHAMA A FUNÇÃO EM OUTRO ARQUIVO
+        if cnn_type == "vgg16":
+            #Lógica pra a VGG16 da Lívia
+            print(f"{cnn_type}-run-{datetime.now().strftime('%H%M')} metrics:")
+
+
+        else:
+            #Treina a ImageNet para o Transfer Learning
+            print(f"Treinando a ImageNet para o Transfer Learning")
+
+
+            if cnn_type == "vgg16-tl":
+                #Lógica para a VGG16 do Artur
+                print(f"{cnn_type}-run-{datetime.now().strftime('%H%M')} metrics:")
+
+            elif cnn_type == "resnet50-tl":
+                #Lógica para a ResNet50
+                print(f"{cnn_type}-run-{datetime.now().strftime('%H%M')} metrics:")
+
+            elif cnn_type == "densenet121-tl":
+                #Lógica para a DenseNet121
+                print(f"{cnn_type}-run-{datetime.now().strftime('%H%M')} metrics:")
+
+            else:
+                print(f"Nenhuma rede foi escolhida para o Transfer Learning, somente a ImageNet rodou")
+
+
+    except Exception as e:
+        print(f"❌ Erro Crítico durante a execução: {e}")
+        wandb.alert(title="Falha no Treino", text=str(e))
+
+    finally:
+        #Fecha a wandB para a próxima run
         run.finish()
 
-    elif(cnn_type == "resnet50"):
-        #lógica da resNet50
 
-        # exportando os dados para o wandb
-        run.log({"accuracy": 0, "loss": 0})
-        run.finish()
-
-    elif(cnn_type == "densenet121"):
-        #lógica da denseNet121
-
-        # exportando os dados para o wandb
-        run.log({"accuracy": 0, "loss": 0})
-        run.finish()
-
-    else:
-        #Nessa caso estamos salvando os dados da imageNet
-        run.log({"accuracy": 0, "loss": 0})
-        run.finish()
+if __name__ == "__main__":
+    main()
 
 
 
